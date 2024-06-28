bbe494be78b230c40c6d7ed5f04062a47229965d
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-06-29 00:12:15 +0300

    Sprite Animation

--- main.py
+++ main.py
@@ -20,6 +20,40 @@ scroll_speed = 4
 bg = pygame.image.load("img/bg.png")
 ground_img = pygame.image.load("img/ground.png")
 
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0 
+        for num in range(1,4):
+            img = pygame.image.load(f"img/bird{num}.png")
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect() 
+        self.rect.center = [x,y]
+
+    def update(self):
+        # handle the animation
+        self.counter += 1
+        flap_cooldown = 5
+
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
+
+
 # Game loop 
 run = True 
 while run:
@@ -28,6 +62,9 @@ while run:
 
     # draw background
     screen.blit(bg, (0,0))
+
+    bird_group.draw(screen)
+    bird_group.update()
 
     #draw and scroll the ground
     screen.blit(ground_img, (ground_scroll,668))
