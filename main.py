1647faa9f208dfeb5643af0433e1e09a1d770e5d
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-06-29 16:04:28 +0300

    Adding a Score Counter

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Flappy Bird")
 
+# define font
+font = pygame.font.SysFont("Bauhaus 93", 60)
+
+#define color
+white = (255,255,255)
+
 # Define game variables
 ground_scroll = 0 
 scroll_speed = 4 
@@ -21,10 +27,16 @@ game_over = False
 pipe_gap = 150
 pipe_frequency = 1500 # milliseconds
 last_pipe = pygame.time.get_ticks() - pipe_frequency
+score = 0
+pass_pipe = False
 
 # Load images
 bg = pygame.image.load("img/bg.png")
 ground_img = pygame.image.load("img/ground.png")
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img,(x,y))
 
 
 class Bird(pygame.sprite.Sprite):
@@ -113,13 +125,24 @@ while run:
 
     bird_group.draw(screen)
     bird_group.update()
-
     pipe_group.draw(screen)
     
-
     # draw the ground
     screen.blit(ground_img, (ground_scroll,668))
+    
+    # check the score
+    if len(pipe_group) > 0:
+        if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left\
+                and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right\
+                and pass_pipe == False:
+                pass_pipe = True
+        if pass_pipe == True:
+            if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+                score += 1
+                pass_pipe = False
 
+    draw_text(str(score), font, white, int(screen_width / 2), 20)
+    
     # look for collision
     if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or  flappy.rect.top < 0:
         game_over = True
